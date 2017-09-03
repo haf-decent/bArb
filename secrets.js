ddf1ef1ad1de06667152d5a469d5332c06b44e72
haf-decent <r.hafner20@gmail.com>
2017-09-03 19:05:37 -0400

    Create secrets.js
    This is used to keep the sensitive private authentication keys and secrets. You can add these by accessing the respective exchange and setting up API access.

---
+++ secrets.js
@@ -0,0 +1,32 @@
+module.exports = {
+  kraken: {
+    key: '',
+    secret: ''
+  },
+  poloniex: {
+    key: '',
+    secret: ''
+  },
+  bittrex: {
+    key: '',
+    secret: ''
+  },
+  gdax: {
+    key: '',
+    secret: '',
+    pass: '',
+    accounts: {
+      "BTC": "",
+      "ETH": "",
+      "LTC": ""
+    }
+  },
+  coinbase: {
+    key: '',
+    secret: ''
+  },
+  bitfinex: {
+    key: '',
+    secret: ''
+  }
+};
